32fdd9df9d9692b1542a937c49b2193efcf0f944
Hark Dhami <harkdhami08@gmail.com>
2026-07-13 13:42:28 +0545

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -128,7 +128,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://property-managementsystem.netlify.app", "http://localhost:5174"],
 
     methods: ["GET", "POST", "PUT", "DELETE"],
 
